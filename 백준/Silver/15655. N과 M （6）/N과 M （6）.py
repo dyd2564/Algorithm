4172b2f3cdf9fb1cc8e3eec7040ea06cca0f7ee0
YongJun-Kim <108705911+dyd2564@users.noreply.github.com>
2023-05-25 20:07:51 +0900

    [Silver III] Title: N과 M (6), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+num = list(map(int, input().split()))
+num.sort()
+arr = []
+
+def dfs(idx, cnt):
+
+    if cnt == m:
+        print(*arr)
+        return
+    for i in range(idx, n):
+        if num[i] not in arr:
+            arr.append(num[i])
+            dfs(i+1, cnt+1)
+            arr.pop()
+dfs(0, 0)
