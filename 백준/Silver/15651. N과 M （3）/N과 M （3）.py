4c8110eb2aea68327fa8f469f90174fa717f2149
YongJun-Kim <108705911+dyd2564@users.noreply.github.com>
2023-05-25 17:41:25 +0900

    [Silver III] Title: N과 M (3), Time: 764 ms, Memory: 157224 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+arr = []
+def dfs(cnt):
+    if cnt == m:
+        print(*arr)
+        return
+    
+    for i in range(1, n+1):
+        arr.append(i)
+        dfs(cnt+1)
+        arr.pop()
+dfs(0)     
