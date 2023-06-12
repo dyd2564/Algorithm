bcbd9ee9aa757304c9f066afbfa5b4c0a16af38a
YongJun-Kim <108705911+dyd2564@users.noreply.github.com>
2023-06-12 10:27:16 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.13 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,21 @@
+def solution(n):
+    answer = 0
+    
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+        
+    
+    a, b = 1, 2
+    for i in range(n-2):
+        a, b = b, a+b
+        
+    
+    
+    
+    
+    
+    return b % 1234567
+
+    
