f39fe2180070463fc786274ce4b75edbe5979f5b
YongJun-Kim <108705911+dyd2564@users.noreply.github.com>
2023-06-15 10:51:03 +0900

    [Silver III] Title: N과 M (3), Time: 2000 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,12 +1,13 @@
 n, m = map(int, input().split())
 arr = []
+num = [i for i in range(1, n+1)]
+
 def dfs(cnt):
     if cnt == m:
         print(*arr)
         return
-    
-    for i in range(1, n+1):
-        arr.append(i)
+    for i in range(n):
+        arr.append(num[i])
         dfs(cnt+1)
         arr.pop()
-dfs(0)     
+dfs(0)
