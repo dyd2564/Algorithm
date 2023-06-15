4875e0ac0efb7367c7be46387c79e994fb55df01
YongJun-Kim <108705911+dyd2564@users.noreply.github.com>
2023-06-15 17:00:15 +0900

    [level 2] Title: 프로세스, Time: 0.42 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.py
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.py
@@ -1,16 +1,21 @@
 from collections import deque
 def solution(priorities, location):
     answer = 0
-    arr = [i for i in range(len(priorities))]
+    arr = [i for i in range(len(priorities))] # 0 1 2 3
     q = deque(priorities) # 3 2 2 1
     p = deque(arr) # 2 3 0 1
     ans = []
+    cnt = 0
     while q:
         max_q = max(q)
         if q[0] != max_q:
             p.append(p.popleft())
             q.append(q.popleft())
         else:
+            cnt += 1
             q.popleft()
-            ans.append(p.popleft())
-    return ans.index(location)+1
+            if p.popleft() == location:
+                break
+            
+            
+    return cnt
