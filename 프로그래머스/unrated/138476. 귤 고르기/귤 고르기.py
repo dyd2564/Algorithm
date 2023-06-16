cbfc68a931509b83df7d2b244481c4bdcc2369fa
YongJun-Kim <108705911+dyd2564@users.noreply.github.com>
2023-06-16 17:07:00 +0900

    [unrated] Title: 귤 고르기, Time: 15.21 ms, Memory: 18.1 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -2,16 +2,11 @@ from collections import Counter
 def solution(k, tangerine):
     answer = 0
     arr = Counter(tangerine)
-    
     arr = sorted(arr.values(), reverse=True)
-    
     cnt = 0
-    
     for i in arr:
-        answer += i
-        cnt += 1
-        if answer >= k:
+        cnt += i
+        answer += 1
+        if cnt >= k:
             break
-        
-    
-    return cnt
+    return answer
