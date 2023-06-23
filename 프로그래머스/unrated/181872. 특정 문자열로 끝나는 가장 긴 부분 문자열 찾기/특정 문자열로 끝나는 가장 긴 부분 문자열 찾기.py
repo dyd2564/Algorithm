61565c7eefbf1f8db4eed12acb2e935ef1eb38f7
YongJun-Kim <108705911+dyd2564@users.noreply.github.com>
2023-06-23 11:24:50 +0900

    [unrated] Title: 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.py
+++ 프로그래머스/unrated/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.py
@@ -1,11 +1,2 @@
 def solution(myString, pat):
-    answer = ''
-    # myString = myString[::-1]
-    # pat = pat[::-1]
-    # if pat in myString:
-    a = []
-    a.append(myString[::-1].find(pat[::-1]))
-    b = (myString[::-1].find(pat[::-1]))
-    if b == 0:
-        return myString
-    return myString[:-b]
+    return myString[:myString.rfind(pat)+len(pat)]
