35b2a69f4aadbd479f5cc95c9961f7198d02163a
YongJun-Kim <108705911+dyd2564@users.noreply.github.com>
2023-07-05 11:25:51 +0900

    [Silver III] Title: 바이러스, Time: 68 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -9,16 +9,28 @@ for i in range(e):
     graph[b].append(a)
 
 visited = [0] * (v+1)
-def bfs(start):
-    q = deque([start])
-    cnt = 0
+# def bfs(start):
+#     q = deque([start])
+#     cnt = 0
+#     visited[start] = 1
+#     while q:
+#         n = q.popleft()
+#         for nx in graph[n]:
+#             if visited[nx] == 0:
+#                 visited[nx] = 1
+#                 q.append(nx)
+#                 cnt += 1
+#     return cnt
+# print(bfs(1))
+cnt = 0
+def dfs(start):
     visited[start] = 1
-    while q:
-        n = q.popleft()
-        for nx in graph[n]:
-            if visited[nx] == 0:
-                visited[nx] = 1
-                q.append(nx)
-                cnt += 1
+    global cnt
+    cnt += 1
+    for nx in graph[start]:
+        if visited[nx] == 0:
+            visited[nx] = 1
+            dfs(nx)
     return cnt
-print(bfs(1))
+
+print(dfs(1)-1)
