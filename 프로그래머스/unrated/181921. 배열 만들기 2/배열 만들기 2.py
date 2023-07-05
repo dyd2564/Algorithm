cd84625bcaad80504be02f2dae7abcac0050c705
YongJun-Kim <108705911+dyd2564@users.noreply.github.com>
2023-07-05 17:27:55 +0900

    [unrated] Title: 배열 만들기 2, Time: 6.18 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
@@ -0,0 +1,15 @@
+def solution(l, r):
+    answer = []
+    for i in range(l, r+1):
+        if i % 5 == 0:
+            a = str(i)
+            flag = True
+            for j in a:
+                if int(j) != 0 and int(j) != 5:
+                    flag = False
+                    break
+            if flag:
+                answer.append(int(a))
+    if len(answer) == 0:
+        return [-1]
+    return answer
