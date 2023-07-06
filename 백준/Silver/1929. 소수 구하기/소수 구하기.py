31376bc7bab564955089698ad7366c0a86c32a9f
YongJun-Kim <108705911+dyd2564@users.noreply.github.com>
2023-07-06 10:56:56 +0900

    [Silver III] Title: 소수 구하기, Time: 5264 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,14 @@
+import sys
+import math
+input = sys.stdin.readline
+
+m, n = map(int, input().split())
+
+for i in range(m, n+1):
+    if i == 1:
+        continue
+    for j in range(2, int(math.sqrt(i))+1):
+        if i % j == 0:
+            break
+    else:
+        print(i)
