142d97ca2152969cb130801fe7f49ac9e57dbe81
YongJun-Kim <108705911+dyd2564@users.noreply.github.com>
2023-07-19 09:59:00 +0900

    [unrated] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
@@ -1,13 +1,12 @@
 def solution(players, callings):
     answer = []
-    play = {rank : i for i, rank in enumerate(players)}
-    # print(play)
+    arr = [i for i in range(len(players))]
+    # print(arr)
+    
     for i in callings:
-        idx = play[i]
-        
-        play[i] -= 1
-        play[players[idx-1]] += 1
-        
-        players[idx-1], players[idx] = players[idx], players[idx-1]
-        
+        arr[players.index(i)] -= 1
+        arr[players.index(i)-1] += 1
+        print(arr)
+    
+    
     return players
