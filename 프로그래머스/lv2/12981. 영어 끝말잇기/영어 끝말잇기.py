b0b723f693afa735e7772391069d19c29ce85680
YongJun-Kim <108705911+dyd2564@users.noreply.github.com>
2023-07-24 12:44:19 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.17 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,12 +1,11 @@
 def solution(n, words):
-    answer = [0, 0]
-    arr = [words[0]]
-    for i in range(1, len(words)):        
-        if arr[-1][-1] == words[i][0] and words[i] not in arr:
-            arr.append(words[i])
+    answer = []
+    word = [words[0]]
+    for i in range(1, len(words)):
+        if word[-1][-1] == words[i][0] and words[i] not in word:
+            word.append(words[i])
         else:
-            answer[0] = (i % n) + 1
-            answer[1] = (i // n) + 1
-            break
+            return [(i%n)+1, (i//n)+1]
     
-    return answer
+
+    return [0, 0]
