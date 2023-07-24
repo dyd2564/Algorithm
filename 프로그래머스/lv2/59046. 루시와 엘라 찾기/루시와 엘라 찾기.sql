f49b7dcd51c6292dc3f1f0ffec78372fda74d3a7
YongJun-Kim <108705911+dyd2564@users.noreply.github.com>
2023-07-24 15:51:34 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,5 +1,4 @@
 -- 코드를 입력하세요
-SELECT animal_id, name, sex_upon_intake
-from animal_ins
-where name in ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
-order by animal_id
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME IN ("Lucy", "Ella", "Pickle", "Rogan", "Sabrina", "Mitty")
