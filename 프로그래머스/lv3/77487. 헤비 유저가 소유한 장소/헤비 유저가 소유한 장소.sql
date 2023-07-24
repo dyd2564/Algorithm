781d1d846429357e37c333167da05e9553d59029
YongJun-Kim <108705911+dyd2564@users.noreply.github.com>
2023-07-24 16:07:35 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,11 +1,9 @@
 -- 코드를 입력하세요
-SELECT a.id, a.name, a.host_id
-from places a 
-join 
-(
-    select id, name, host_id
-    from places
-    group by host_id
-    having count(*) >= 2
-) b
-on a.host_id = b.host_id
+SELECT ID, NAME, HOST_ID
+FROM PLACES
+WHERE HOST_ID IN (
+    SELECT HOST_ID
+    FROM PLACES
+    GROUP BY HOST_ID
+    HAVING COUNT(*) >= 2
+)
