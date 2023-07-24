bf4eaac0e589b0e35df277867c302befcb8a6577
YongJun-Kim <108705911+dyd2564@users.noreply.github.com>
2023-07-24 10:33:46 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.25 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,11 +1,10 @@
 def solution(A,B):
     answer = 0
-    a = len(A)
+
     A.sort()
     B.sort(reverse=True)
-    if len(A) < len(B):
-        a = len(B)
-    for i in range(a):
-        answer += A[i]*B[i]
+    
+    for i in range(len(A)):
+        answer += A[i] * B[i]
 
     return answer
